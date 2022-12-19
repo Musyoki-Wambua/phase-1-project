8403669815f8b40b83d48189556fe5e02442ffc4
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2022-12-19 17:49:35 +0300

    alert user when search result is invalid

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   const character = {
     name: "Spider-Man",
     description: "Spider-Man is a superhero who fights crime in New York City.",
     thumbnail: {
       path: "https://www.sideshow.com/storage/product-images/500985FBLK/the-amazing-spider-man-800_marvel_feature.jpg",
-      extension: "jpg"
-    }
+      extension: "jpg",
+    },
   };
 
   const loginForm = document.getElementById("login");
@@ -15,11 +14,11 @@ document.addEventListener("DOMContentLoaded", () => {
   loginForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-     // Check if the form has already been submitted
-  if (loginForm.style.display === "none") {
-    // If the form has already been submitted, do nothing
-    return;
-  }
+    // Check if the form has already been submitted
+    if (loginForm.style.display === "none") {
+      // If the form has already been submitted, do nothing
+      return;
+    }
     //get email and password values
     const email = loginForm.elements.email.value;
     const password = loginForm.elements.password.value;
@@ -32,82 +31,89 @@ document.addEventListener("DOMContentLoaded", () => {
     loginForm.style.display = "none";
   });
 
-// Get the search form element
-const searchForm = document.getElementById('search-form');
+  // Get the search form element
+  const searchForm = document.getElementById("search-form");
 
-// Add a submit event listener to the search form
-searchForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+  // Add a submit event listener to the search form
+  searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  // Get the search input value
-const searchInput = document.getElementById('search-input');
+    // Get the search input value
+    const searchInput = document.getElementById("search-input");
 
-  // Get the search query from the search input element
-  const searchQuery = searchInput.value;
+    // Get the search query from the search input element
+    const searchQuery = searchInput.value;
 
     // Clear the search input element
     searchInput.value = "";
 
+    // Search for characters matching the search query
+    searchCharacters(searchQuery);
+  });
+
   // Search for characters matching the search query
-  searchCharacters(searchQuery);
+  function searchCharacters(searchQuery) {
+    const searchUrl = `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchQuery}&ts=1&apikey=fbba092e440b12f9973140191f427182&hash=3760cd56b1bf07ff1707f3c7b0092c3d`;
+
+    // Fetch characters from the Marvel API
+    fetch(searchUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // Get the results array from the data object
+        const results = data.data.results;
+
+        // Filter the results by the search query
+        const filteredResults = results.filter((character) => {
+          // Check if the character's name includes the search query
+          return character.name
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase());
+        });
+
+        // Clear the character list container
+        const characterListContainer =
+          document.getElementById("character-list");
+        characterListContainer.innerHTML = "";
+
+        // If no filtered results, alert the user
+      if (filteredResults.length === 0) {
+        alert(`No characters found in database with the name "${searchQuery}"`);
+        characterListContainer.innerHTML = `<button id="back-button">Back</button>`;
+        const backButton = document.getElementById("back-button");
+        backButton.addEventListener("click", () => {
+          location.reload();          
 });
-
-  // Search for characters matching the search query
-function searchCharacters(searchQuery) {
-  const searchUrl = (`https://gateway.marvel.com/v1/public/characters?nameStartsWith=${searchQuery}&ts=1&apikey=fbba092e440b12f9973140191f427182&hash=3760cd56b1bf07ff1707f3c7b0092c3d`);
-  
-  // Fetch characters from the Marvel API
-  fetch(searchUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      // Get the results array from the data object
-      const results = data.data.results;
-
-      // Filter the results by the search query
-      const filteredResults = results.filter((character) => {
-
-        // Check if the character's name includes the search query
-        return character.name.toLowerCase().includes(searchQuery.toLowerCase());
-      });
-
-      // Clear the character list container
-      const characterListContainer = document.getElementById('character-list');
-      characterListContainer.innerHTML = "";
-      
-      for (let i = 0; i < filteredResults.length; i++) {
-        renderCharacter(filteredResults[i]);
       }
-    })
+
+
+        for (let i = 0; i < filteredResults.length; i++) {
+          renderCharacter(filteredResults[i]);
+        }
+      });
   }
-      //render a character
-    function renderCharacter(character) {
-      // Get the character list container element
-      const characterListContainer = document.getElementById("character-list");
-    
-      // Create a new div element to contain the character data
-      const characterDiv = document.createElement("div");
-      characterDiv.classList.add("character");
-      characterDiv.style.width= '400px';
-      characterDiv.style.height= '400px';
+  //render a character
+  function renderCharacter(character) {
+    // Get the character list container element
+    const characterListContainer = document.getElementById("character-list");
 
-      // Add the character name to the div element
-      characterDiv.innerHTML = `<h2>${character.name}</h2>`;
-    
-      // Add the character description to the div element
-      characterDiv.innerHTML += `<p>${character.description}</p>`;
-    
-      // Add the character image to the div element
-      characterDiv.innerHTML += `<img src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}" style="width: 400px; height: 400px;">`;
-    
-      // Append the character div to the character list container
-      characterListContainer.appendChild(characterDiv);
+    // Create a new div element to contain the character data
+    const characterDiv = document.createElement("div");
+    characterDiv.classList.add("character");
+    characterDiv.style.width = "400px";
+    characterDiv.style.height = "400px";
+    characterDiv.style.clear = 'left'
 
-      const container = document.querySelector('.character-container');
-      const characterWidth = 200;
-      const containerWidth = container.offsetWidth;
-      const characterPerRow = math.floor(containerWidth/characterWidth);
-      const containerHeight = math.cell(character.length/ charcterPerRow) * characterWidth;
-      container,style.height = `${containerHeight} px`;
-    }renderCharacter()
+    // Add the character name to the div element
+    characterDiv.innerHTML = `<h2>${character.name}</h2>`;
 
+    // Add the character image to the div element
+    characterDiv.innerHTML += `<img id= "characterImage"src="${character.thumbnail.path}.${character.thumbnail.extension}" alt="${character.name}" style="float:left; width: 400px; height: 400px;">`;
+
+    // Add the character description to the div element
+    characterDiv.innerHTML += `<p id='textDescription' >${character.description}</p>`;
+
+    // Append the character div to the character list container
+    characterListContainer.appendChild(characterDiv);
+  }
+  renderCharacter();
 });
