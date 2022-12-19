d48b177c9d9d21c3f20fd1adb81bcea9d1763af5
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2022-12-19 06:46:40 +0300

    Configured my API URL

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  const loginForm = document.getElemtById('login-form')
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.getElementById("login-form");
 
   //trigger event listner that get email adn password values
-  loginForm.addEventListener (('submit'), (event) => {
+  loginForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
+    //get email and password values
     const email = loginForm.target.email.value;
     const password = loginForm.target.password.value;
 
+    if (email == "test@example.com" && password == "password") {
+      alert("LOGIN SUCESSFUL!!");
+    } else alert("LOGIN SUCESSFUL!!");
+
     //after login is done, form is closed
-    loginForm.style.display = 'none'
+    loginForm.style.display = "none";
+  });
 
+  const publicKey = "b38c9d8d4620433cb70fe81c8f2fda8e";
+  const privateKey = "495c242bcacbbdc5214e22b0eea0ff4e99a21f6e";
 
+  const timestamp = "1";
+  const hash = md5(timestamp + privateKey + publicKey);
 
-  })
+  const endpoint = "https://gateway.marvel.com/v1/public/characters";
+  const params = `apikey=${publicKey}&ts=${timestamp}&hash=${hash}`;
+  
+  const url = `${endpoint}?${params}`;
 
-  fetch ('https://gateway.marvel.com/v1/public/characters?apikey=b38c9d8d4620433cb70fe81c8f2fda8e')
-  .then (response => response.json())
- // .then ((data) => console.log(data))
+  fetch('https://gateway.marvel.com/v1/public/characters?ts=1&apikey=b38c9d8d4620433cb70fe81c8f2fda8e&hash=5302dfea25f63a3f3b06c96ba8df6571')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 
-  console.log(jncnj)
+});
 
-})
